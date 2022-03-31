654d0e650e3eab431c538c7652ef2df4e78b44f9
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-03-31 22:45:54 +0200

    feat: display text entered in textarea

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,69 +1,29 @@
 import { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import './App.styles.scss';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      markdown: '',
     };
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  increment() {
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
-  }
-
-  decrement() {
-    this.setState(
-      (state) => (state.count > 0 && { count: state.count - 1 }) || state
-    );
-  }
-
-  reset() {
-    this.setState(() => ({ count: 0 }));
+  handleChange(e) {
+    e.preventDefault();
+    const markdown = e.target.value;
+    this.setState({ markdown });
   }
 
   render() {
-    const { count } = this.state;
+    const { markdown } = this.state;
     return (
-      <div id="counter">
-        <h3>
-          <span>Counter: </span>
-          {count}
-        </h3>
-        <div>
-          <button type="button" onClick={this.increment}>
-            <FontAwesomeIcon
-              icon={solid('plus')}
-              size="sm"
-              style={{ marginRight: '0.4em' }}
-            />
-            Increment
-          </button>
-          <button type="button" onClick={this.decrement}>
-            <FontAwesomeIcon
-              icon={solid('minus')}
-              size="sm"
-              style={{ marginRight: '0.4em' }}
-            />
-            Decrement
-          </button>
-          <button type="button" onClick={this.reset}>
-            <FontAwesomeIcon
-              icon={solid('xmark')}
-              size="sm"
-              style={{ marginRight: '0.4em' }}
-            />
-            Reset
-          </button>
-        </div>
+      <div id="container">
+        <textarea onChange={this.handleChange} name="editor" id="editor" />
+        <p id="preview">{markdown}</p>
       </div>
     );
   }
