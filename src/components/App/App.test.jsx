9843376dee28fa4c3f51693e0c8a7a837e6baf8a
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-03-31 22:23:47 +0200

    test(tdd): add app component tests

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,54 +1,93 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-describe('Count incrementation', () => {
-  it('should increment counter when clicking on increment button', () => {
-    render(<App />);
-    const title = screen.getByRole('heading', { name: /counter/i });
-    const incButton = screen.getByRole('button', { name: /increment/i });
+beforeAll(() => {
+  global.markdown = `
+      # I am a heading element
 
-    expect(title).toHaveTextContent('Counter: 0');
-    fireEvent.click(incButton);
-    expect(title).toHaveTextContent('Counter: 1');
-  });
+      ## I am a sub heading element
+
+      [I am a link](https://github.com/lamai6/markdown-previewer)
+
+      I am a ~inline code~
+
+      ~~~js
+      console.log('I am a line of code')
+      ~~~
+
+      - I am a list item
+      - Me too
+      - **I am a bolded list item**
+
+      > I am a blockquote
+
+      ![I am an image](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`;
 });
 
-describe('Count decrementation', () => {
-  it('should decrement counter when clicking on decrement button', () => {
-    render(<App />);
-    const title = screen.getByRole('heading', { name: /counter/i });
-    const incButton = screen.getByRole('button', { name: /increment/i });
-    const decButton = screen.getByRole('button', { name: /decrement/i });
-
-    fireEvent.click(incButton);
-    expect(title).toHaveTextContent('Counter: 1');
-    fireEvent.click(decButton);
-    expect(title).toHaveTextContent('Counter: 0');
-  });
-
-  it('should not decrement counter under 0 when clicking on decrement button', () => {
-    render(<App />);
-    const title = screen.getByRole('heading', { name: /counter/i });
-    const decButton = screen.getByRole('button', { name: /decrement/i });
-
-    expect(title).toHaveTextContent('Counter: 0');
-    fireEvent.click(decButton);
-    expect(title).toHaveTextContent('Counter: 0');
-  });
+afterAll(() => {
+  global.markdown = undefined;
 });
 
-describe('Count reset', () => {
-  it('should reset counter when clicking on reset button', () => {
-    render(<App />);
-    const title = screen.getByRole('heading', { name: /counter/i });
-    const incButton = screen.getByRole('button', { name: /increment/i });
-    const resButton = screen.getByRole('button', { name: /reset/i });
+describe('App component tests', () => {
+  it('should render a textarea element with a corresponding id="editor" (US #1)', () => {
+    const { getByRole } = render(<App />);
+    const textarea = getByRole('textbox');
 
-    fireEvent.click(incButton);
-    fireEvent.click(incButton);
-    expect(title).toHaveTextContent('Counter: 2');
-    fireEvent.click(resButton);
-    expect(title).toHaveTextContent('Counter: 0');
+    expect(textarea).toBeInTheDocument();
+  });
+
+  it('should render an element with a corresponding id="preview" (US #2)', () => {
+    const { container } = render(<App />);
+    const markdownPreview = container.querySelector('p[id=preview]');
+
+    expect(markdownPreview).toBeInTheDocument();
+  });
+
+  it('should update preview section when entering text into editor section (US #3)', () => {
+    const { container, getByRole } = render(<App />);
+    const textarea = getByRole('textbox');
+    const markdownPreview = container.querySelector('p[id=preview]');
+
+    expect(markdownPreview).toHaveTextContent('');
+
+    fireEvent.change(textarea, {
+      target: { value: '# Here is a markdown heading !' },
+    });
+
+    expect(markdownPreview).toHaveTextContent('# Here is a markdown heading !');
+  });
+
+  it('should render markdown as HTML in preview section when entering markdown into editor section (US #4)', () => {
+    const { container, getByRole } = render(<App />);
+    const textarea = getByRole('textbox');
+    const markdownPreview = container.querySelector('p[id=preview]');
+
+    fireEvent.change(textarea, {
+      target: { value: global.markdown },
+    });
+
+    expect(markdownPreview.children).toHaveLength(8);
+  });
+
+  it('should contain valid markdown in editor section on page load (US #5)', () => {
+    const { getByRole } = render(<App />);
+    const textarea = getByRole('textbox');
+
+    expect(textarea).toHaveTextContent(global.markdown);
+  });
+
+  it('should render markdown as HTML in preview section on page load (US #6)', () => {
+    const { container } = render(<App />);
+    const markdownPreview = container.querySelector('p[id=preview]');
+
+    expect(markdownPreview.children).toHaveLength(8);
+  });
+
+  it('should interpret carriage returns and renders them as <br /> elements (US #7)', () => {
+    const { container } = render(<App />);
+    const markdownPreview = container.querySelectorAll('br');
+
+    expect(markdownPreview).toHaveLength(7);
   });
 });
