129ff84ad82482eaf4a1e97f0e4bba522432c21f
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-03 16:33:14 +0200

    refactor(testing): use same markdown example as app component in tests

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,28 +1,10 @@
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
+import markdown from './App.markdown';
 
 beforeAll(() => {
-  global.markdown = `
-      # I am a heading element
-
-      ## I am a sub heading element
-
-      [I am a link](https://github.com/lamai6/markdown-previewer)
-
-      I am a ~inline code~
-
-      ~~~js
-      console.log('I am a line of code')
-      ~~~
-
-      - I am a list item
-      - Me too
-      - **I am a bolded list item**
-
-      > I am a blockquote
-
-      ![I am an image](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`;
+  global.markdown = markdown;
 });
 
 afterAll(() => {
