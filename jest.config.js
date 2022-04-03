b2bf3533e01144c1ace1d0d3b910da30d214879f
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-03 16:39:50 +0200

    fix(testing): transform react-markdown esm lib with babel for testing purposes

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,27 @@
+const esModules = [
+  'react-markdown',
+  'vfile',
+  'unist-.+',
+  'unified',
+  'bail',
+  'is-plain-obj',
+  'trough',
+  'remark-.+',
+  'mdast-util-.+',
+  'micromark',
+  'parse-entities',
+  'character-entities',
+  'property-information',
+  'comma-separated-tokens',
+  'hast-util-whitespace',
+  'remark-.+',
+  'space-separated-tokens',
+  'decode-named-character-reference',
+  'ccount',
+  'escape-string-regexp',
+  'markdown-table',
+].join('|');
+
 module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -5,4 +29,10 @@ module.exports = {
     '\\.(scss|css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
   },
   testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    `[/\\\\]node_modules[/\\\\](?!${esModules}).+\\.(js|jsx|mjs|cjs|ts|tsx)$`,
+  ],
 };
